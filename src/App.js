fc71337f7223a6dc7ed0729c74903ef2eff6ab82
Vadim <chakapega@gmail.com>
2019-07-23 18:18:52 +0300

    Creating Header, HomePage components and styling them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HomePage from './components/HomePage';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
-  }
+      <Router>
+        <Header />
+        <Route path='/' exact component={HomePage} />
+      </Router>
+    );
+  };;
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <span className='header__logo'>My rating of movies and serials</span> 
+    </div>
+  );
+};

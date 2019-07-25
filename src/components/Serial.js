90b4530a5ffbf7b805d2c1919a9bb48985be16ab
Vadim <chakapega@gmail.com>
2019-07-26 01:08:33 +0300

    created Serials and Serial components and also added styles for them

---
+++ src/components/Serial.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Serial.css';
+
+export default function Serial({ serial, openPosterFullSize}) {
+  return (
+    <div className='serial__container'>
+      <div className='serial__description_container'>
+        <h3 className='serial__title'>{serial.name}</h3>
+        <span className='serial__year'>{serial.year}</span>
+        <p className='serial__description'>{serial.description}</p>
+        <span className='serial__rating'>{serial.rating}</span>
+      </div>
+      <div className='serial__poster_container'>
+        <img className='serial__poster' src={serial.posterUrl} alt='serial-poster' width='360' onClick={openPosterFullSize}></img>
+      </div>
+    </div>
+  );
+};
--- src/components/SerialsPage.js
+++ src/components/SerialsPage.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import Serials from './Serials';
+import PosterFullSize from './PosterFullSize';
 
 export default class SerialsPage extends Component {
+  state = {
+    serials: [],
+    isOpenPosterFullSize: false,
+    posterFullSizeUrl: ''
+  };
+
+  componentDidMount() {
+    const serials = [];
+
+    window.db.collection('serials').get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(serial => {
+          const newSerial = serial.data();
+          newSerial.id = serial.id;
+
+          serials.push(newSerial);
+        });
+        this.setState({serials: serials});
+      });
+  };
+
+  openPosterFullSize = e => {
+    const { isOpenPosterFullSize } = this.state;
+
+    this.setState({
+      isOpenPosterFullSize: !isOpenPosterFullSize,
+      posterFullSizeUrl: e.target.src
+    });
+  };
+
   render() {
+    const { serials, isOpenPosterFullSize, posterFullSizeUrl } = this.state;
+    const posterFullSize = isOpenPosterFullSize && <PosterFullSize openPosterFullSize={this.openPosterFullSize} posterFullSizeUrl={posterFullSizeUrl} />
+
     return (
       <div className='serials__page'>
-        
+        {posterFullSize}
+        <Serials openPosterFullSize={this.openPosterFullSize} serials={serials} />
       </div>
     );
   };
